39bf42ade7c0ac1dc770d392e3725ccbd57dba96
Jose Alberto Garcia Sanchez <jagasanchez@gmail.com>
2020-03-28 21:21:48 +0100

    Adding addtional directory

---
+++ cmake-build-debug/CMakeFiles/perc.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/perc.dir/main.c.o"
+  "perc"
+  "perc.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/perc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
